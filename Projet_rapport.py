0ed676550ff111ab96a84eb96b39e2be0d798b7b
kenyboka <keny.boka1@gmail.com>
2026-03-30 14:40:39 -0400

    Update Projet_rapport.py

--- Projet_rapport.py
+++ Projet_rapport.py
@@ -19,13 +19,13 @@ def ecrire_rapport(chemin, mois, devise,total, comparaison):
     """
     ecrire le rapport dans un fichier texte.
     """
-    with open (chemin, "w") as r:
+    with open (chemin, "w") as B:
 
-        r.write(f"\nle rapport du mois de {mois}")
-        r.write(f"\nle total des depenses est : {total} {devise}")
-        r.write(f"\nCategorie / Depense / limite / etat")
+        B.write(f"\nle rapport du mois de {mois}")
+        B.write(f"\nle total des depenses est : {total} {devise}")
+        B.write(f"\nCategorie / Depense / limite / etat")
     
         for categorie, montant, limite, etat in comparaison:
-            r.write(f"\n{categorie} / {montant} / {limite} / {etat}")
+            B.write(f"\n{categorie} / {montant} / {limite} / {etat}")
             ligne = "-" * 50
-            r.write(ligne + "\n")
+            B.write(ligne + "\n")
