13237f37f0d2bbd93f178e0cc89716a9ffb7c6b4
kenyboka <keny.boka1@gmail.com>
2026-03-30 19:29:45 -0400

    Create Projet_lecture.py

---
+++ Projet_lecture.py
@@ -0,0 +1,46 @@
+"""
+Gestion de budget
+Auteur: Joseph Boka
+"""
+import csv 
+import json
+ 
+def lire_csv(chemin):
+    """
+    lire un fichier CSV contenant les dépenses
+    """
+    Liste_depenses = {}
+    try:
+
+        with open(chemin, "r") as fichier:
+            lecteur = csv.DictReader(fichier)
+
+        # Lire chaque ligne du CSV
+    
+        for ligne in lecteur:
+            categorie = ligne["description"]
+            montant = float(ligne["montant"])
+            Liste_depenses[categorie] =  Liste_depenses.get(categorie, 0) + montant
+
+
+    except FileNotFoundError:
+        print("Erreur : fichier introuvable")
+    except ValueError:
+        print("erreur : montant invalide dans le CSV")
+
+    return Liste_depenses
+
+def lire_json(chemin):
+    """
+    lire un fichier JSON contenant le budget.
+    """
+    try: 
+        with open(chemin, "r") as f:
+            return json.load(f)
+    except FileNotFoundError:
+        print("erreur : fichier JSON introuvable.")
+        return {}
+    except json.JSONDecodeError:
+        print("erreur : JSON mal formé.")
+        return {}
+
